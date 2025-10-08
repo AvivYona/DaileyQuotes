af5e6791d63d133bc005b31692632cceb6ee8db5
aviv <avivyona17@gmail.com>
2025-10-08 15:44:42 +0300

    fix delete

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -3,9 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { Author, AuthorSchema } from '../schemas/author.schema';
+import { Quote, QuoteSchema } from '../schemas/quote.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Author.name, schema: AuthorSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Author.name, schema: AuthorSchema },
+      { name: Quote.name, schema: QuoteSchema },
+    ]),
+  ],
   controllers: [AuthorsController],
   providers: [AuthorsService],
   exports: [AuthorsService],
